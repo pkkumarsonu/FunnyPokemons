999895cf5ee0ecbedec4aac10a47efa001f8c8b5
Prateek Kumar <prateekx.kumar@intel.com>
2022-02-21 21:09:33 +0530

    Adding new child component

---
+++ src/Components/PokemonThumbnail.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+
+const PokemonThumbnail = ({
+  id,
+  name,
+  image,
+  type,
+  height,
+  weight,
+  stat1,
+  stat2,
+  stat3,
+  stat4,
+  stat5,
+  stat6,
+  bs1,
+  bs2,
+  bs3,
+  bs4,
+  bs5,
+  bs6,
+}) => {
+  const [show, setShow] = useState(false);
+  return (
+    <div>
+      <div>
+        <small>#0{id}</small>
+      </div>
+      <img src={image} alt={name} />
+      <div>
+        <h3>{name.toUpperCase()}</h3>
+        <small>Type : {type}</small>
+        <button onClick={() => setShow(!show)}>
+          {show === true ? "Know less..." : "Know more..."}
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default PokemonThumbnail;
