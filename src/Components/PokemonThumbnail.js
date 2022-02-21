9b159941afff128db69b68bd41608b1e59099554
Prateek Kumar <prateekx.kumar@intel.com>
2022-02-21 21:57:26 +0530

    passing data to Description component

--- src/Components/PokemonThumbnail.js
+++ src/Components/PokemonThumbnail.js
@@ -34,6 +34,26 @@ const PokemonThumbnail = ({
         <button onClick={() => setShow(!show)}>
           {show === true ? "Know less..." : "Know more..."}
         </button>
+        {show === true ? (
+          <Description
+            weightpok={weight}
+            heightpok={height}
+            pokstat1={stat1}
+            pokstat2={stat2}
+            pokstat3={stat3}
+            pokstat4={stat4}
+            pokstat5={stat5}
+            pokstat6={stat6}
+            posbs1={bs1}
+            posbs2={bs2}
+            posbs3={bs3}
+            posbs4={bs4}
+            posbs5={bs5}
+            posbs6={bs6}
+          />
+        ) : (
+          <></>
+        )}
       </div>
     </div>
   );
