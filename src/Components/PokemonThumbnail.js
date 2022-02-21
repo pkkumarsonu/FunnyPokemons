63846c59bb3fce20568ed3231965a30b47ba059a
Prateek Kumar <prateekx.kumar@intel.com>
2022-02-21 22:38:04 +0530

    Adding style to PokemonThumbnail

--- src/Components/PokemonThumbnail.js
+++ src/Components/PokemonThumbnail.js
@@ -21,17 +21,18 @@ const PokemonThumbnail = ({
   bs5,
   bs6,
 }) => {
+  const style = `thumb-container ${type}`;
   const [show, setShow] = useState(false);
   return (
-    <div>
-      <div>
+    <div className={style}>
+      <div className="number">
         <small>#0{id}</small>
       </div>
       <img src={image} alt={name} />
-      <div>
+      <div className="detail-wrapper">
         <h3>{name.toUpperCase()}</h3>
         <small>Type : {type}</small>
-        <button onClick={() => setShow(!show)}>
+        <button className="pokeinfo" onClick={() => setShow(!show)}>
           {show === true ? "Know less..." : "Know more..."}
         </button>
         {show === true ? (
