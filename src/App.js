130e8697c499fa8a3a7585dad8b4016870704798
Prateek Kumar <prateekx.kumar@intel.com>
2022-02-21 20:22:45 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from "react";
+
+function App() {
+  const [allPokemons, setAllPokemons] = useState([]);
+  const [loadPoke, setLoadPoke] = useState(
+    "https://pokeapi.co/api/v2/pokemon?limit=20"
+  );
+  const getAllPokemons = async () => {
+    const res = await fetch(loadPoke);
+    const data = await res.json();
+    setLoadPoke(data.next);
+
+    function createPokemonObject(result) {
+      result.forEach(async (pokemon) => {
+        const res = await fetch(
+          `https://pokeapi.co/api/v2/pokemon/${pokemon.name}`
+        );
+        const data = await res.json();
+        setAllPokemons((currentList) => [...currentList, data]);
+      });
+    }
+    createPokemonObject(data.results);
+    await console.log(allPokemons);
+  };
+  useEffect(() => {
+    getAllPokemons();
+  }, []);
+
+  return (
+    <div>
+      <h1>Pokemon-Cards</h1>
+
+      <div>
+        <button onClick={() => getAllPokemons()}>More Pokemons</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
